e9d8bced838ccbad4f201f3f6c7849cf7f617ef5
someOne-ai <anonymousinduty@gmail.com>
2024-08-06 10:41:54 +0530

    Update 9a_mutate_select_filter.R

--- PART A/9a_mutate_select_filter.R
+++ PART A/9a_mutate_select_filter.R
@@ -12,24 +12,19 @@ print(mutate(stats, avg = runs / 4))
 # Drop all and create a new column
 print(transmute(stats, avg = runs / 4))
 
-# ii. Import dplyr package
-library(dplyr)
-
 # Create a data frame
-stats <- data.frame(player = c('A', 'B', 'C', 'D'),
+stats1 <- data.frame(player = c('A', 'B', 'C', 'D'),
                     runs = c(100, 200, 408, 19),
                     wickets = c(17, 20, NA, 5))
 
 # Fetch required column data
-print(select(stats, player, wickets))
+print(select(stats1, player, wickets))
 
-# iii. Import dplyr package
-library(dplyr)
 
 # Create a data frame
-stats <- data.frame(player = c('A', 'B', 'C', 'D'),
+stats2 <- data.frame(player = c('A', 'B', 'C', 'D'),
                     runs = c(100, 200, 408, 19),
                     wickets = c(17, 20, NA, 5))
 
 # Fetch players who scored more than 100 runs
-print(filter(stats, runs > 100))
+print(filter(stats2, runs > 100))
